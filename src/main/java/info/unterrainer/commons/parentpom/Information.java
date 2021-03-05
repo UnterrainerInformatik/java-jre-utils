48e982feacfc2dafa0036831ca3706d4a58069ad
Psilo <psilo@unterrainer.info>
2021-03-05 07:56:06 +0100

    add DateUtils

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-12-10T07:22:32Z";
-	public static final String pomVersion = "0.1.2";
+	public static final String buildTime = "2021-01-15T14:20:00Z";
+	public static final String pomVersion = "0.1.3";
 }
