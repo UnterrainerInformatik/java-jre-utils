c12e21c6aafc2bf5c80ac0c160df68e09d727204
Psilo <psilo@unterrainer.info>
2021-03-10 09:20:36 +0100

    repair information mechanism and bump version

--- src/main/java/info/unterrainer/commons/jreutils/Information.java
+++ src/main/java/info/unterrainer/commons/jreutils/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.jreutils;
 
 public class Information {
 	public static final String name = "JRE-Utils";
-	public static final String buildTime = "2021-03-10T08:12:59Z";
-	public static final String pomVersion = "0.1.6";
+	public static final String buildTime = "2021-03-10T08:20:20Z";
+	public static final String pomVersion = "0.1.7";
 }
