921f53c8b67dd9b9476305428aee8a35196f147b
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2021-09-30 14:58:45 +0200

    update tests

--- src/test/java/info/unterrainer/commons/jreutils/ExceptionsTests.java
+++ src/test/java/info/unterrainer/commons/jreutils/ExceptionsTests.java
@@ -1,11 +1,14 @@
 package info.unterrainer.commons.jreutils;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class ExceptionsTests {
 
 	@Test
-	public void BeforeTest() {
+	public void codeBeforeTest() {
 		try {
 			// Some method you don't have control over throwing an exception.
 			throw new IllegalArgumentException();
@@ -15,9 +18,18 @@ public class ExceptionsTests {
 	}
 
 	@Test
-	public void AfterTest() {
+	public void throwsExceptionWithoutSwallow() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			// Some method you don't have control over throwing an exception.
+			throw new IllegalArgumentException();
+		});
+	}
+
+	@Test
+	public void swallowsException() {
 		Exceptions.swallow(() -> {
 			throw new IllegalArgumentException();
 		}, IllegalArgumentException.class, NumberFormatException.class);
+		assertTrue(true);
 	}
 }
