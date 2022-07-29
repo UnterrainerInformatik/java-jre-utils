2bd9861958e8243ab40dbe18e24c7af13603460d
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2022-07-29 15:05:39 +0200

    fix microsecond-omission

--- src/main/java/info/unterrainer/commons/jreutils/DateUtils.java
+++ src/main/java/info/unterrainer/commons/jreutils/DateUtils.java
@@ -7,6 +7,7 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.WeekFields;
 
 import lombok.experimental.UtilityClass;
@@ -21,7 +22,7 @@ public class DateUtils {
 	}
 
 	public static Long utcLocalDateTimeToEpoch(final LocalDateTime utc) {
-		return utc.atZone(ZoneId.of("UTC")).toInstant().toEpochMilli();
+		return ChronoUnit.MICROS.between(Instant.EPOCH, utc.atZone(ZoneId.of("UTC")).toInstant());
 	}
 
 	public static LocalDateTime epochToUtcLocalDateTime(final Long epoch) {
